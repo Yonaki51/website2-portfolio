7e7805276bc7973f29d4021e4602ffc3e43305b2
Yonaki51 <yonivandon@hotmail.com>
2025-12-10 08:33:41 +0100

    functie toegevoegd voor smooth scrollen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-const snippetText = 
-`const developer = {
+const snippetText =
+  `const developer = {
   name: "yoni",
   skills: ["java", "html/css", "python"],
   passion: "solving problems"
@@ -8,13 +8,31 @@ const snippetText =
 let snippetIndex = 0;
 
 function typeSnippet() {
-    const box = document.getElementById("snippet");
-    box.textContent = snippetText.slice(0, snippetIndex);
-    snippetIndex++;
+  const box = document.getElementById("snippet");
+  box.textContent = snippetText.slice(0, snippetIndex);
+  snippetIndex++;
 
-    if (snippetIndex <= snippetText.length) {
-        setTimeout(typeSnippet, 70); // snelheid per teken
-    }
+  if (snippetIndex <= snippetText.length) {
+    setTimeout(typeSnippet, 70); // snelheid per teken
+  }
 }
 
 window.addEventListener("DOMContentLoaded", typeSnippet);
+
+// dropdown
+document.querySelectorAll(".skill-header").forEach(header => {
+    header.addEventListener("click", () => {
+        
+        const clickedItem = header.parentElement;
+
+        // Sluit ALLE skill-items behalve degene waarop is geklikt
+        document.querySelectorAll(".skill-item").forEach(item => {
+            if (item !== clickedItem) {
+                item.classList.remove("active");
+            }
+        });
+
+        // Toggle alleen het geklikte item
+        clickedItem.classList.toggle("active");
+    });
+});
